c37a4c70f5d8ea398bdeb08ff572ec23ffa57e07
Francis Tan <francis@cycurid.com>
2022-06-15 16:32:22 -0700

    hi

---
+++ README.md
@@ -0,0 +1,5 @@
+IMPORT IN REACT:
+import { immeOauth, immeLogout } from "imme";
+
+IMPORT IN NODE:
+const { getToken, getUserInfo, refreshToken, revokeToken } = require("imme");
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const OAUTH_SERVER = "https://api.cycurid.com";
-const IMMEWIDGET_URL = `http://localhost:3000`;
+const IMMEWIDGET_URL = `https://accounts.van.cycurid.com`;
 
 exports.IMMEWIDGET_URL = IMMEWIDGET_URL;
 exports.OAUTH_SERVER = OAUTH_SERVER;
