43c16ae23af38e5ea459ba00b02695aed0aa6ac6
Jordan <developer@cycurid.com>
2022-06-17 09:24:20 -0700

    Updated ImmeWidget Url to production

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const OAUTH_SERVER = "https://api.cycurid.com";
-const IMMEWIDGET_URL = `https://accounts.van.cycurid.com`;
+const IMMEWIDGET_URL = `https://accounts.imme.io`;
 
 exports.IMMEWIDGET_URL = IMMEWIDGET_URL;
 exports.OAUTH_SERVER = OAUTH_SERVER;
