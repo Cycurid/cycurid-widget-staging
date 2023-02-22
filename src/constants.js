fc0c71b73e14d846145710b8df7a81f8884d3b08
Jordan <developer@cycurid.com>
2023-02-22 14:48:39 -0800

    staging constants added

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-const OAUTH_SERVER = "https://api.cycurid.com";
-const IMMEWIDGET_URL = `https://accounts.imme.io`;
+const OAUTH_SERVER = "https://api.van.cycurid.com";
+const IMMEWIDGET_URL = `https://accounts.van.cycurid.com/`;
 
 exports.IMMEWIDGET_URL = IMMEWIDGET_URL;
 exports.OAUTH_SERVER = OAUTH_SERVER;
